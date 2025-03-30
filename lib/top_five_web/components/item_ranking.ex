8958c66f6efaf7af75c9b20a3e4edaedb26995db
Jason Florentino <jason.florentino@relayfi.com>
2025-03-30 19:05:32 -0400

    handle submit of chooser list

--- lib/top_five_web/components/item_ranking.ex
+++ lib/top_five_web/components/item_ranking.ex
@@ -1,51 +1,63 @@
 defmodule TopFiveWeb.ItemRankingComponent do
+  alias TopFiveWeb.ButtonComponent
   use Phoenix.LiveComponent
   import TopFive.Helpers
 
   def render(assigns) do
     ~H"""
-    <ol class="w-full list-none flex flex-col gap-4">
-      <%= if not Enum.empty?(@keys) do %>
-        <%= for {key, idx} <- Enum.with_index(@keys) do %>
-          <% item_data = Enum.find(@key_data, fn item -> item.key == key end) %>
-          <li class="flex gap-3 items-center">
-            <button
-              phx-target={@myself}
-              phx-click="change_rank"
-              phx-value-item_key={item_data[:key]}
-              phx-value-rank_old={idx}
-              phx-value-rank_new={idx - 1}
-              class={
-                cn([
-                  "py-2 px-3 bg-sky-400 rounded-full text-lg",
-                  idx == 0 && "opacity-0 pointer-events-none"
-                ])
-              }
-            >
-              ⬆️
-            </button>
-            <button
-              phx-target={@myself}
-              phx-click="change_rank"
-              phx-value-item_key={item_data[:key]}
-              phx-value-rank_old={idx}
-              phx-value-rank_new={idx + 1}
-              class={
-                cn([
-                  "py-2 px-3 bg-sky-700 rounded-full text-lg",
-                  idx == 4 && "opacity-0 pointer-events-none"
-                ])
-              }
-            >
-              ⬇️
-            </button>
-            <span class="font-medium">
-              {idx + 1}. {item_data[:name]}
-            </span>
-          </li>
+    <div class="flex flex-col gap-4">
+      <ol class="w-full list-none flex flex-col gap-4">
+        <%= if not Enum.empty?(@keys) do %>
+          <%= for {key, idx} <- Enum.with_index(@keys) do %>
+            <% item_data = Enum.find(@key_data, fn item -> item.key == key end) %>
+            <li class="flex gap-3 items-center">
+              <button
+                phx-target={@myself}
+                phx-click="change_rank"
+                phx-value-item_key={item_data[:key]}
+                phx-value-rank_old={idx}
+                phx-value-rank_new={idx - 1}
+                class={
+                  cn([
+                    "py-2 px-3 bg-sky-400 rounded-full text-lg",
+                    idx == 0 && "opacity-0 pointer-events-none"
+                  ])
+                }
+              >
+                ⬆️
+              </button>
+              <button
+                phx-target={@myself}
+                phx-click="change_rank"
+                phx-value-item_key={item_data[:key]}
+                phx-value-rank_old={idx}
+                phx-value-rank_new={idx + 1}
+                class={
+                  cn([
+                    "py-2 px-3 bg-sky-700 rounded-full text-lg",
+                    idx == 4 && "opacity-0 pointer-events-none"
+                  ])
+                }
+              >
+                ⬇️
+              </button>
+              <span class="font-medium">
+                {idx + 1}. {item_data[:name]}
+              </span>
+            </li>
+          <% end %>
         <% end %>
-      <% end %>
-    </ol>
+      </ol>
+
+      <form phx-submit="handle_submit" phx-target={@myself}>
+        <.live_component
+          module={ButtonComponent}
+          id="submit-top-five"
+          class="min-w-fit"
+          text="Lock in my top five"
+        />
+      </form>
+    </div>
     """
   end
 
@@ -60,5 +72,13 @@ defmodule TopFiveWeb.ItemRankingComponent do
       |> List.insert_at(String.to_integer(rank_new), item_key)
 
     {:noreply, assign(socket, keys: keys)}
+  end
+
+  def handle_event("handle_submit", _params, socket) do
+    keys =
+      Map.get(socket.assigns, :keys)
+
+    IO.inspect(keys, label: "jason:submit:keys")
+    {:noreply, socket}
   end
 end
